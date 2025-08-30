35f9258762642c152ee086c27072410c2e9b657a
bichnq <bichnq@deha-soft.com>
2025-08-30 22:41:29 +0700

    Add Poppins font family and configure app themes
    - Added Poppins font files to the assets/fonts directory.
    - Created colors.dart for color constants.
    - Defined light and dark themes in themes.dart using the new color constants.
    - Updated main.dart to use the defined themes and set SplacePage as the home.
    - Implemented SplacePage with a basic Scaffold layout.

---
+++ lib/config/themes.dart
@@ -0,0 +1,36 @@
+import 'package:chat_app/config/colors.dart';
+import 'package:flutter/material.dart';
+
+var lightTheme = ThemeData();
+var darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  useMaterial3: true,
+  colorScheme: const ColorScheme.dark(
+    primary: dPrimaryColor,
+    onPrimary: dOnBackGroundColor,
+    surface: dBackGroundColor,
+    onSurface: dOnContainerColor,
+    primaryContainer: dContainerColor,
+    onPrimaryContainer: dOnContainerColor,
+  ),
+  textTheme: TextTheme(
+    headlineLarge: TextStyle(
+      fontSize: 32,
+      color: dPrimaryColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w800,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 30,
+      color: dOnBackGroundColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: TextStyle(
+      fontSize: 20,
+      color: dOnBackGroundColor,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w600,
+    ),
+  ),
+);
